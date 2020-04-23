9fef5f49b23c944adabd72bd6ed525a78dac375b
taoqf <tao_qiufeng@126.com>
2020-04-23 17:30:06 +0800

    remove n

--- src/get-params.ts
+++ src/get-params.ts
@@ -1,8 +1,6 @@
-import { HTMLElement as NHTMLElement } from 'node-html-parser';
-
 const PARAMS_FLAG = 'data-mm-props';
 
-export default function get_params(node: HTMLElement | NHTMLElement) {
+export default function get_params(node: HTMLElement) {
 	const params_str = node.attributes[PARAMS_FLAG];
 	return params_str ? JSON.parse(`${params_str}`) : {};
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export { default as bp } from './b-page';
 export { default as bc } from './b-component';
-export { default as np } from './n-page';
-export { default as nc } from './n-component';
